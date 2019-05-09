6219791071dd1ecbc214e34614e02084ab3c94ec
Safintim <Sarami96>
2019-05-09 17:42:39 +0300

    Changed function of receiving data

--- devman_bot.py
+++ devman_bot.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 
 class SecretData:
     def __init__(self):
-        self.token_devman = os.getenv('TOKEN_DEVMAN')
-        self.token_bot = os.getenv('TOKEN_TELEGRAM')
-        self.chat_id = os.getenv('CHAT_ID')
+        self.token_devman = os.environ('TOKEN_DEVMAN')
+        self.token_bot = os.environ('TOKEN_TELEGRAM')
+        self.chat_id = os.environ('CHAT_ID')
 
 
 class Message:
