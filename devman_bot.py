47904df4f97df2b7a39fdb6515c61cca2088b2b8
Safintim <Sarami96>
2019-05-12 23:55:25 +0300

    Fix

--- devman_bot.py
+++ devman_bot.py
@@ -65,7 +65,7 @@ class BotDevman:
             handler = logs_handler()
             handler.setFormatter(logging.Formatter('%(message)s'))
             logger.addHandler(handler)
-            logger.setLevel(logging.DEBUG)
+            logger.setLevel(logging.INFO)
             return logger
 
     def request_to_devman(self, params):
